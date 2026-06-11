f1e36e0217ea297c1602d8e489f8febccf87a613
fahridzalasidiq-droid <fahridzal.asidiq@mhs.unsoed.ac.id>
2026-06-11 09:51:00 +0700

    Update |  |-- 01_analisis_teks.R

--- |-- src_R/|  |-- 01_analisis_teks.R
+++ |-- src_R/|  |-- 01_analisis_teks.R
@@ -1,1 +1,55 @@
+# PROGRAM 1
+# Menghitung jumlah kata dan jumlah kalimat
+
+# SKENARIO 1 (NORMAL)
+
+teks <- "Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini dikarenakan jejaring sosial memiliki manfaat yang sangat banyak bagi usaha bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat promosi yang paling efektif."
+
+jumlah_kata <- length(strsplit(teks, "\\s+")[[1]])
+
+jumlah_kalimat <- length(strsplit(teks, "\\.")[[1]]) - 1
+
+cat("Skenario 1\n")
+## Skenario 1
+cat(
+  "Teks tersebut memuat",
+  jumlah_kalimat,
+  "kalimat dan",
+  jumlah_kata,
+  "kata.\n\n"
+)
+# SKENARIO 2 (DATA LEBIH SEDIKIT)
+
+teks <- "Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis."
+
+jumlah_kata <- length(strsplit(teks, "\\s+")[[1]])
+
+jumlah_kalimat <- length(strsplit(teks, "\\.")[[1]]) - 1
+
+cat("Skenario 2\n")
+## Skenario 2
+cat(
+  "Teks tersebut memuat",
+  jumlah_kalimat,
+  "kalimat dan",
+  jumlah_kata,
+  "kata.\n\n"
+)
+# SKENARIO 3 (DATA MENENGAH)
+
+teks <- "Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa."
+
+jumlah_kata <- length(strsplit(teks, "\\s+")[[1]])
+
+jumlah_kalimat <- length(strsplit(teks, "\\.")[[1]]) - 1
+
+cat("Skenario 3\n")
+## Skenario 3
+cat(
+  "Teks tersebut memuat",
+  jumlah_kalimat,
+  "kalimat dan",
+  jumlah_kata,
+  "kata.\n"
+)
 
