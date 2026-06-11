a9cc33048323d95ad5e3634375554d520c4292c8
fahridzalasidiq-droid <fahridzal.asidiq@mhs.unsoed.ac.id>
2026-06-12 00:26:23 +0700

    Update |    |-- 01_analisis_teks.py

--- |-- src_python/|    |-- 01_analisis_teks.py
+++ |-- src_python/|    |-- 01_analisis_teks.py
@@ -1,13 +1,44 @@
 # PROGRAM 1
-# Menghitung jumlah kata dan jumlah kalimat
+# Menghitung Jumlah Kata dan Jumlah Kalimat
+
+
+# Menghitung Jumlah Kata
+
+def hitung_kata(teks):
+
+    jumlah = 1
+
+    for huruf in teks:
+
+        if huruf == " ":
+
+            jumlah = jumlah + 1
+
+    return jumlah
+
+
+# Menghitung Jumlah Kalimat
+
+def hitung_kalimat(teks):
+
+    jumlah = 0
+
+    for huruf in teks:
+
+        if huruf == ".":
+
+            jumlah = jumlah + 1
+
+    return jumlah
+
 
 # SKENARIO 1 (NORMAL)
 
 teks = "Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini dikarenakan jejaring sosial memiliki manfaat yang sangat banyak bagi usaha bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat promosi yang paling efektif."
 
-jumlah_kata = len(teks.split())
+jumlah_kata = hitung_kata(teks)
 
-jumlah_kalimat = len(teks.split(".")) - 1
+jumlah_kalimat = hitung_kalimat(teks)
 
 print("Skenario 1")
 
@@ -22,13 +53,13 @@ print(
 print()
 
 
-# SKENARIO 2 (DATA LEBIH SEDIKIT)
+# SKENARIO 2 (NORMAL)
 
 teks = "Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis."
 
-jumlah_kata = len(teks.split())
+jumlah_kata = hitung_kata(teks)
 
-jumlah_kalimat = len(teks.split(".")) - 1
+jumlah_kalimat = hitung_kalimat(teks)
 
 print("Skenario 2")
 
@@ -43,13 +74,13 @@ print(
 print()
 
 
-# SKENARIO 3 (DATA MENENGAH)
+# SKENARIO 3 (KONDISI KHUSUS)
 
-teks = "Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa."
+teks = "Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat promosi yang paling efektif."
 
-jumlah_kata = len(teks.split())
+jumlah_kata = hitung_kata(teks)
 
-jumlah_kalimat = len(teks.split(".")) - 1
+jumlah_kalimat = hitung_kalimat(teks)
 
 print("Skenario 3")
 
@@ -60,5 +91,3 @@ print(
     jumlah_kata,
     "kata."
 )
-
-
