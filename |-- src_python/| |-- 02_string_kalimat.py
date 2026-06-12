e672cd9e9a5db5da637b36efeef85f34a8f1f971
Sarah Alimunnisa <sarah.alimunnisa@mhs.unsoed.ac.id>
2026-06-12 13:24:36 +0700

    Update | |-- 02_string_kalimat.py

--- |-- src_python/| |-- 02_string_kalimat.py
+++ |-- src_python/| |-- 02_string_kalimat.py
@@ -1,1 +1,15 @@
+# PROGRAM 2
+# Membuat objek string K1 sampai K4
 
+K1 = "Saya tak 'kan menyerah."
+
+K2 = 'Ia berkata, "Aku menyayangimu."'
+
+K3 = "\"Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!\""
+
+K4 = "Surat ayer le itu bernomor 62/UN.34/19/2023."
+
+print(K1)
+print(K2)
+print(K3)
+print(K4)
