084b7fa13f9bfc3de3ebb8259a341394388b1a8c
Sarah Alimunnisa <sarah.alimunnisa@mhs.unsoed.ac.id>
2026-06-16 18:28:20 +0700

    Update | |-- 03_akar_kuadrat.R

--- |-- src_R/| |-- 03_akar_kuadrat.R
+++ |-- src_R/| |-- 03_akar_kuadrat.R
@@ -1,79 +1,94 @@
-# PROGRAM 3
-# Akar-akar Persamaan Kuadrat
-# Input koefisien persamaan kuadrat a,b,c
-# Hitung diskriminan
-#misalkan persamaan kuadratnya:x^2+6x-9
-# a=1, b=6, c=9
-a<-1;b<-6;c<- -9
-diskriminan <- b^2 - 4*a*c
-# Cek nilai diskriminan
-if (diskriminan > 0) {
-  x1 <- (-b + sqrt(diskriminan)) / (2*a)
-  x2 <- (-b - sqrt(diskriminan)) / (2*a)
-  cat("Persamaan memiliki dua akar real berbeda:\n")
-  cat(sprintf("x1 = %.3f\n", x1))
-  cat(sprintf("x2 = %.3f\n", x2))
-} else if (diskriminan == 0) {
-  x <- -b / (2*a)
-  cat("Persamaan memiliki akar real kembar:\n")
-  cat(sprintf("x1 = x2 = %.3f\n", x))
-} else {
-  cat("Persamaan hanya memiliki akar-akar imajiner.\n")
+#PROGRAM NOMOR 3
+#MELAKUKAN PERHITUNGAN TERHADAP PERSAMAAN KUADRAT
+#misalkan 0x+2x+3
+a <- 0; b <- 2; c <-3
+#Mengecek nilai koefisien a
+if (a == 0){
+  print("Bukan persamaan kuadrat")
+}else {
+  #Menghitung diskriminan
+  diskriminan <- b^2 - 4*a*c
+  #mengecek nilai diskriminan
+  if (diskriminan > 0) {
+    x1 <- (-b + sqrt(diskriminan)) / (2*a)
+    x2 <- (-b - sqrt(diskriminan)) / (2*a)
+    cat("Persamaan memiliki dua akar real berbeda:\n")
+    cat(sprintf("x1 = %.3f\n", x1))
+    cat(sprintf("x2 = %.3f\n", x2))
+  } else if (diskriminan == 0) {
+    x <- -b / (2*a)
+    cat("Persamaan memiliki akar real kembar:\n")
+    cat(sprintf("x1 = x2 = %.3f\n", x))
+  } else {
+    cat("Persamaan hanya memiliki akar-akar imajiner.\n")
+  }
 }
-
 # SKENARIO 1 (DUA AKAR REAL BERBEDA)
-a <- 1; b <- -5; c <- 6
-diskriminan <- b^2 - 4*a*c
-cat("Skenario 1\n")
-## Skenario 1
-if (diskriminan > 0) {
-  x1 <- (-b + sqrt(diskriminan)) / (2*a)
-  x2 <- (-b - sqrt(diskriminan)) / (2*a)
-  cat("Persamaan memiliki dua akar real berbeda.\n")
-  cat(sprintf("x1 = %.3f\n", x1))
-  cat(sprintf("x2 = %.3f\n", x2))
-} else if (diskriminan == 0) {
-  x <- -b / (2*a)
-  cat("Persamaan memiliki dua akar real kembar.\n")
-  cat(sprintf("x1 = x2 = %.3f\n", x))
-} else {
-  cat("Persamaan hanya memiliki akar-akar imajiner.\n")
+a <- 1; b <- -5; c <-6
+#Mengecek nilai koefisien a
+if (a == 0){
+  print("Bukan persamaan kuadrat")
+}else {
+  #Menghitung diskriminan
+  diskriminan <- b^2 - 4*a*c
+  #mengecek nilai diskriminan
+  if (diskriminan > 0) {
+    x1 <- (-b + sqrt(diskriminan)) / (2*a)
+    x2 <- (-b - sqrt(diskriminan)) / (2*a)
+    cat("Persamaan memiliki dua akar real berbeda:\n")
+    cat(sprintf("x1 = %.3f\n", x1))
+    cat(sprintf("x2 = %.3f\n", x2))
+  } else if (diskriminan == 0) {
+    x <- -b / (2*a)
+    cat("Persamaan memiliki akar real kembar:\n")
+    cat(sprintf("x1 = x2 = %.3f\n", x))
+  } else {
+    cat("Persamaan hanya memiliki akar-akar imajiner.\n")
+  }
 }
-cat("\n")
 # SKENARIO 2 (DUA AKAR REAL KEMBAR)
 a <- 1; b <- -4; c <- 4
-diskriminan <- b^2 - 4*a*c
-cat("Skenario 2\n")
-## Skenario 2
-if (diskriminan > 0) {
-  x1 <- (-b + sqrt(diskriminan)) / (2*a)
-  x2 <- (-b - sqrt(diskriminan)) / (2*a)
-  cat("Persamaan memiliki dua akar real berbeda.\n")
-  cat(sprintf("x1 = %.3f\n", x1))
-  cat(sprintf("x2 = %.3f\n", x2))
-} else if (diskriminan == 0) {
-  x <- -b / (2*a)
-  cat("Persamaan memiliki dua akar real kembar.\n")
-  cat(sprintf("x1 = x2 = %.3f\n", x))
-} else {
-  cat("Persamaan hanya memiliki akar-akar imajiner.\n")
+#Mengecek nilai koefisien a
+if (a == 0){
+  print("Bukan persamaan kuadrat")
+}else {
+  #Menghitung diskriminan
+  diskriminan <- b^2 - 4*a*c
+  #mengecek nilai diskriminan
+  if (diskriminan > 0) {
+    x1 <- (-b + sqrt(diskriminan)) / (2*a)
+    x2 <- (-b - sqrt(diskriminan)) / (2*a)
+    cat("Persamaan memiliki dua akar real berbeda:\n")
+    cat(sprintf("x1 = %.3f\n", x1))
+    cat(sprintf("x2 = %.3f\n", x2))
+  } else if (diskriminan == 0) {
+    x <- -b / (2*a)
+    cat("Persamaan memiliki akar real kembar:\n")
+    cat(sprintf("x1 = x2 = %.3f\n", x))
+  } else {
+    cat("Persamaan hanya memiliki akar-akar imajiner.\n")
+  }
 }
-cat("\n")
 # SKENARIO 3 (AKAR IMAJINER)
 a <- 1; b <- 2; c <- 5
-diskriminan <- b^2 - 4*a*c
-cat("Skenario 3\n")
-## Skenario 3
-if (diskriminan > 0) {
-  x1 <- (-b + sqrt(diskriminan)) / (2*a)
-  x2 <- (-b - sqrt(diskriminan)) / (2*a)
-  cat("Persamaan memiliki dua akar real berbeda.\n")
-  cat(sprintf("x1 = %.3f\n", x1))
-  cat(sprintf("x2 = %.3f\n", x2))
-} else if (diskriminan == 0) {
-  x <- -b / (2*a)
-  cat("Persamaan memiliki dua akar real kembar.\n")
-  cat(sprintf("x1 = x2 = %.3f\n", x))
-} else {
-  cat("Persamaan hanya memiliki akar-akar imajiner.\n")
+#Mengecek nilai koefisien a
+if (a == 0){
+  print("Bukan persamaan kuadrat")
+}else {
+  #Menghitung diskriminan
+  diskriminan <- b^2 - 4*a*c
+  #mengecek nilai diskriminan
+  if (diskriminan > 0) {
+    x1 <- (-b + sqrt(diskriminan)) / (2*a)
+    x2 <- (-b - sqrt(diskriminan)) / (2*a)
+    cat("Persamaan memiliki dua akar real berbeda:\n")
+    cat(sprintf("x1 = %.3f\n", x1))
+    cat(sprintf("x2 = %.3f\n", x2))
+  } else if (diskriminan == 0) {
+    x <- -b / (2*a)
+    cat("Persamaan memiliki akar real kembar:\n")
+    cat(sprintf("x1 = x2 = %.3f\n", x))
+  } else {
+    cat("Persamaan hanya memiliki akar-akar imajiner.\n")
+  }
 }
